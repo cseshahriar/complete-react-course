5b32611d10ec8b99c31e8a072b7abc334ab76082
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-08-24 09:11:51 +0600

    html css dynamic

---
+++ class_2_var/variables.js
@@ -0,0 +1,43 @@
+/*
+4 Ways to Declare a JavaScript Variable
+Using var
+Using let
+Using const
+Using nothing
+*/
+
+// What are Variables?
+// Variables are containers for storing data (storing data values).
+var person_name = "shahriar";
+console.log("name ", person_name);
+
+/*
+When to Use JavaScript var?
+    Always declare JavaScript variables with var,let, or const.
+    The var keyword is used in all JavaScript code from 1995 to 2015.
+    The let and const keywords were added to JavaScript in 2015.
+
+    If you want your code to run in older browser, you must use var.
+
+    When to Use JavaScript const?
+        If you want a general rule: always declare variables with const.
+        If you think the value of the variable can change, use let.
+        In this example, price1, price2, and total, are variables:
+/* */
+let person = "John Doe", carName = "Volvo", price = 200;
+
+
+/**
+* JavaScript types
+The set of types in the JavaScript language consists of primitive values and objects.
+Primitive values (immutable datum represented directly at the lowest level of the language)
+
+Boolean type
+Null type
+Undefined type
+Number type
+BigInt type
+String type
+Symbol type
+Objects (collections of properties)
+*/
--- core-concepts/src/App.js
+++ core-concepts/src/App.js
@@ -2,21 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // variables
+  var name ="Shahriar";
+  // objects
+  var person = {
+    name: "Shahriar",
+    job: "Programmer",
+    age: 29,
+  }
+
+  var style = {
+    color: 'green',
+    fontSize: '60px',
+    border: '1px solid green',
+    padding: '15px',
+    borderRadius: '5px',
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={logo} className="App-logo" alt="logo"/>
+
+        <h1  style={style}>My first react heading</h1>
+        <p style={{backgroundColor: 'cyan', color: 'black', padding:'10px', borderRadius: '5px'}}>
+          My first react paragraph
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Expression { 2 + 3}</p>
+        <p>Expression {(2+5)*25+65}</p>
+        <p>Name { name }</p>
+        <p>Name {person.name}, Job {person.job}, Age {person.age}</p>
       </header>
     </div>
   );
