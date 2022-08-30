d56c750ca048aba9550b406c5b6d40f073945821
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-08-30 06:10:36 +0600

    js object

---
+++ class_3/reference_type.js
@@ -0,0 +1,29 @@
+// Data Type || Reference
+
+// Object 
+// Array
+// Function
+
+// object 
+let info = {
+    "name": "Shahriar",
+    "age": 29,
+    "bloodGroup": "B+",
+    "isMarried": true
+
+} // {} object letterers
+
+// dot notation or object notation
+console.log(typeof info);
+console.log(info);
+
+info.address = "Dhaka";
+console.log(info);
+console.log(info.bloodGroup);
+
+// bracket notation
+let key = 'phone';
+let value = "01710835653";
+info[key] = value;
+console.log(info);
+
