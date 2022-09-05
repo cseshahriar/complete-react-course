d77e3a520742e90fdc217980359f38c3ce26c99a
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-09-05 18:31:36 +0600

    shallow copy and deep copy

--- class_4/pass_by_ref.js
+++ class_4/pass_by_ref.js
@@ -8,3 +8,51 @@ function increment(number) {
 }
 increment(number)
 console.log(number); // { value: 11 }
+
+
+// ==============  shallow copy ===============
+const car = { // car properties can be changed for reference type when its const
+    color: 'red',
+    brand: 'BWD',
+    size: {
+        length: 10, 
+        width: 5
+    }
+}
+const secondCar = car; // shallow copy / অগভীর অনুলিপি
+car.wheel = 4
+car.type = "sedan"
+
+secondCar.body = 'something';
+
+console.log(car);
+
+/* {
+  color: 'red',
+  brand: 'BWD',
+  wheel: 4,
+  type: 'sedan',
+  body: 'something'
+}*/
+
+console.log(secondCar);
+/* 
+{
+  color: 'red',
+  brand: 'BWD',
+  wheel: 4,
+  type: 'sedan',
+  body: 'something'
+}
+*/
+
+// ============ shallow copy solution deep copy =============
+// const thirdCar = {};
+// for(let i in car) {
+//     thirdCar[i] = car[i]
+// }
+const thirdCar = Object.assign({}, car);
+thirdCar.any = "anything";
+console.log(thirdCar);
+
+
