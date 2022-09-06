fc9a652b13dce5e331c8d7faa5943983d874d232
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-09-06 20:44:56 +0600

    component and props

--- recap/src/App.js
+++ recap/src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 
 // component is reusable 
 function App() {
+  // data
+  const nayoks = ['Jashim', 'Deepjol', 'Bapparaz'];
+
   return (
     <div className="App">
-      <Nayok></Nayok>
-      <Nayok></Nayok>
-      <Nayok></Nayok>
+      <Nayok name='Jasim' age='20'></Nayok>
+      <Nayok name='Sakib' age='20'></Nayok>
+      <Nayok name='Salman' age=''></Nayok>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
@@ -17,7 +20,7 @@ function App() {
 }
 
 // component
-function Nayok() {
+function Nayok(props) {
   const nayokStyle = {
     border: '2px solid purple',
     margin: '20px',
@@ -25,8 +28,8 @@ function Nayok() {
   }
 
   return <div style={nayokStyle}>
-    <h1>Ami Khol Nayak</h1> 
-    <p>I have done 5 movies</p>
+    <h1>Ami Nayok {props.name}</h1> 
+    <p>I have done 5 movies in {props.age || 30} years</p>
   </div> 
 }
 
