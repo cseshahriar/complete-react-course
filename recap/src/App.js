7a10a5dba9d349c1f9a293220a41466c8b0d14ec
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-09-07 21:16:12 +0600

    country api call and render

--- recap/src/App.js
+++ recap/src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {useEffect, useState} from 'react';
 
 // component is reusable 
 function App() {
-  // data
-  // const nayoks = ['Jashim', 'Deepjol', 'Bapparaz'];
-
   const [users, setUsers] = useState([]);
 
   // data load 
@@ -14,12 +10,29 @@ function App() {
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
     .then(data => setUsers(data))
-  }, []) 
+    .catch(error => console.log(error));
+  }, []);
+
+
+  
+  // country api fetch
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => { 
+    fetch("https://restcountries.com/v2/all")
+    .then(response => response.json())
+    .then(
+      data => {
+        setCountries(data)
+      }
+    )
+    .catch(error => console.log(error));
+  }, [])
+
 
 
   return (
     <div className="App">
-
       <header className="App-header">
       {
         users.map(user => <Nayok name={user.name} age={user.age} key={user.id}></Nayok>)
@@ -29,9 +42,12 @@ function App() {
         <Nayok name='Salman' age=''></Nayok> */}
         
         <MovieCounter></MovieCounter>
-
+        
+        <h1>Country loaded: {countries.length}</h1>
+        {
+          countries.map(country => <Country country={country} key={country.alpha3Code}></Country>)
+        }
       </header>
-
     </div>
   );
 }
@@ -75,6 +91,31 @@ function MovieCounter() {
 
 function MovieDisplay(props) {
   return <h4>Movies I have acted: {props.count} </h4>
+}
+
+function Country(props) {
+
+  const countryStyle = {
+    border: '2px solid purple',
+    margin: '20px',
+    padding: '20px',
+    width:"400px",
+  }
+  const flagStyle = {
+    width: "250px",
+    header: "100px"
+  }
+  // destructuring
+  const {name, population, region, flag} = props.country
+  // console.log(props.country);
+  
+  return <div style={countryStyle}>
+    <h5>This is a {name}</h5>
+    <img src={flag} alt={name} style={flagStyle} />
+    <p>Population: {population}</p>
+    <p>Region: {region}</p>
+
+  </div>
 }
 
 
