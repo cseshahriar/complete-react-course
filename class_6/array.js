28c2c95585f15af8bf40b8ab73f184f3e769e173
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-09-09 12:27:57 +0600

    array operation

--- class_6/array.js
+++ class_6/array.js
@@ -17,3 +17,54 @@ console.log(numbers); // [2, 3, 4,  5, 6, 7, 8, 9, 10]
 numbers.splice(3, 1,) // start el, how much
 console.log(numbers); // [2, 3, 4,  6, 7, 8, 9, 10] 5 is removed
 
+let another = numbers;
+// numbers.length = 0;
+// console.log(numbers); // []
+
+// all remove
+numbers.splice(0, numbers.length);
+console.log(numbers); // []
+
+
+// combine
+let numbers1 = [1, 2, 3, 4, 5]
+let another1 = [6, 7, 8, 10, 11];
+let combine = numbers1.concat(another1)
+console.log(combine);
+
+
+console.log(another1); // [ 6, 7, 8, 10, 11 ]
+const sliceData = another1.slice(0, 3);  // [ 6, 7, 8 ]
+console.log(sliceData);
+
+const copyNumbers = [...numbers1] // copy by spread
+console.log(copyNumbers); // numbers1
+
+
+// sorting
+let nonSortnumbers = [1, 5, 10, 2, 3, 7, 4];
+const manualSort = function(a, b) { // ascending order
+    // a === b => 0
+    if(a < b) return -1;
+    if(a > b) return 1;
+    return 0;
+}
+let sortedNumbers = nonSortnumbers.sort(manualSort); // callback function accept
+console.log(sortedNumbers);
+
+
+let courses = [
+    {id:1 , name: 'reactjs'},
+    {id:3 , name: 'nodejs'},
+    {id:2 , name: 'angular'},
+    {id:4 , name: 'js'},
+];
+
+const compare = (a, b) => {
+    if(a.name < b.name) return -1;
+    if(a.name > b.name) return 1;
+    return 0;
+}
+let sorted_courses = courses.sort(compare)
+console.log(sorted_courses);
+
