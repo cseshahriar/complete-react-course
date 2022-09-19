069a2959c4cc57d8ccf55565afd9ad9086788c97
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-09-19 14:55:20 +0600

    scope chaining

--- class_9/scope.js
+++ class_9/scope.js
@@ -1,22 +1,34 @@
-/**
-* What is scope?
-* Scope can be defined as the space in which variables and statements are accessible. It makes it possible to have variables with the same name without colliding with one another and prevents outer scopes from having access to inner scopes.
-* In Javascript we have three types of scope: global scope, function/local scope and block scope. Let's learn what those are:
-*/
-
-/**
- * Global scope
- * The default scope
- * There is only one global scope in the program
- * It is the top scope that englobes the entire code
- * The declarations inside this space can be accessed anywhere in the code
- */
-
-const age = 28;
-function getAge() {
-    return age;
+// scope is execution context memory space
+// scope have own memory environment and parent lexical environment
+// lexical environment ?
+var b = 10;
+function a() {
+    function c() {
+        console.log(b);
+    }
+    c();
 }
-console.log(getAge()); // 28
-/**
- * The function getAge is able to reference the age that is outside the function but located in the global scope.
+a();
+
+
+/** c -> a -> global this scope chaining |
+ *           c() execution context       | 
+ *           ||
+ *           a() execution context       |
+ *            |                          
+ *           global execution context    |
+ *                                       |
+ *  ============ call stack =============
  */
+
+
+
+
+
+
+
+
+
+
+
+
