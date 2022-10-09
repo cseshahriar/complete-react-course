57e41dee6dc2be0bbec495ca3861acfdcf2d8606
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-09 09:43:56 +0600

    js execution blocking solve with async js

--- class_13/sync_js_problem.js
+++ class_13/sync_js_problem.js
@@ -1,3 +1,4 @@
+// synchronous js execution line by line
 // js synchronous problem: execution stop
 const processOrder = (customer) => {
     console.log('Processing order for customer 1');
@@ -23,3 +24,33 @@ console.log('completed order for customer 1');
  * 
  * completed order for customer 1
  */
+
+
+// blocking problem solution with asynchronous js
+const processedOrder2 = (customer) => {
+    console.log('2 Processing order for customer 1');
+
+    // setTimeout call back argument func after 3 seconds
+    setTimeout(() => {
+        console.log('2 cooking completed');
+    }, 3000);
+
+    console.log('2 order processed for customer 1');  
+} 
+
+console.log('2 take order for customer 1');
+processedOrder2()
+console.log('2 completed order for customer 1');
+
+/**
+ * take order for customer 1
+ * 
+ * Processing order for customer 1
+ * order processed for customer 1
+ * 
+ * completed order for customer 1 // not blocking
+ * 
+ * cooking completed
+*/
+
+// callback queue
