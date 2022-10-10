a0b9f5e70e7e216621e67976d1d2d06a6a73b61d
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-10 08:31:43 +0600

    component layers

--- react-core-features/src/components/app.jsx
+++ react-core-features/src/components/app.jsx
@@ -1,6 +1,20 @@
 import React, {Component} from "react";
 
 class App extends Component {
+    // component have to layer 1. data 2. representation
+
+    // 1.Data and logic layer (optional)
+    /**
+    * 1. State
+    * 2. Variable or Properties
+    * 3. Function or Methods
+    * 4. Life Cycle methods
+    */
+
+    // 2.Representation
+    /**
+    * 5. JSX
+    */
     render() {
         return <h1>Hello React, You are awesome</h1>
     }
