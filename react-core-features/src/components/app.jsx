dd8f18025a09bf16b0804bf462fb3a03af023e0e
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-10 09:37:41 +0600

    functional component

--- react-core-features/src/components/app.jsx
+++ react-core-features/src/components/app.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 
-class App extends Component {
+class App extends Component { // class base/stateful, smart component
     // component have to layer 1. data 2. representation
 
     // 1.Data and logic layer (optional)
@@ -21,3 +21,22 @@ class App extends Component {
 }
 
 export default App;
+
+// there are two different components
+// 1. class base component
+// 2. function base component
+
+
+// functional component has no data layer
+// functional component
+// functional component is also called dump component
+// only for view then use functional component
+// functioal component use when make ui generation no logic
+function MyFunctionBaseComponent() {
+    const name = "shahriar";
+    return <h1>Hello React, You are awesome {{ name }}</h1>
+}
+
+const AnotherFunctionalComponent = () => {
+    <h1>Hello React, You are awesome</h1>
+}
