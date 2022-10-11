92b0575e84c8cdd491da35471acd367f4961795c
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-11 08:18:03 +0600

    basic jsx use

--- react-core-features/src/components/app.jsx
+++ react-core-features/src/components/app.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 
+const MyComponent = () => <h1>My Component</h1>;
+
 class App extends Component { // class base/stateful, smart component
     // component have to layer 1. data 2. representation
 
@@ -16,7 +18,25 @@ class App extends Component { // class base/stateful, smart component
     * 5. JSX
     */
     render() {
-        return <h1>Hello React, You are awesome</h1>
+        // jsx
+        const obj = {
+            id:1,
+            title: 'title 2'
+        }
+        const name = "Md Shahriar Hosen";
+        const bio = <div>
+            <h3>{name}</h3>
+            <p>Fullstack Developer</p>
+        </div>
+
+        return (<div>
+                <h1 title="title" {...obj}>Hello React, You are awesome</h1>
+                <MyComponent/>
+                <p>{ new Date().toDateString() }</p>
+                <p>My name {name}</p>
+                { bio }
+            </div>
+        )
     }
 }
 
@@ -31,7 +51,7 @@ export default App;
 // functional component
 // functional component is also called dump component
 // only for view then use functional component
-// functioal component use when make ui generation no logic
+// functional component use when make ui generation no logic
 function MyFunctionBaseComponent() {
     const name = "shahriar";
     return <h1>Hello React, You are awesome {{ name }}</h1>
