515b9e034356bafaef8a3e256b7d265852b07d3c
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-10-12 20:13:59 +0600

    email method fix

--- class_14_hd/solve_promise.js
+++ class_14_hd/solve_promise.js
@@ -48,17 +48,6 @@ function getTopMovies() {
     })
 }
 
-  
-function sendEmail(movies) {
-    // send email for customer
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(movies)
-        })
-    })
-}
-
-
 function sendEmail(email, movies) {
     return new Promise(function (resolve, reject) {
         setTimeout(() => {
