a12f513cd46f4ab2bc65677a6a1c2d22fa9637d5
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-12 19:55:31 +0600

    dashboard component

---
+++ class-21_and_22_func_component_router/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/dashboard/dashboard.component';
+
+
+// pure function
+const App = () => {
+  
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
