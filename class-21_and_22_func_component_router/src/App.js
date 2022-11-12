d3834652ecf301dae29199f374206d04d4966191
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-12 20:18:08 +0600

    navbar component

--- class-21_and_22_func_component_router/src/App.js
+++ class-21_and_22_func_component_router/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Dashboard from './components/dashboard/dashboard.component';
-
+import AppNavbar from './components/navbar/navbar.component';
 
 // pure function
 const App = () => {
   
   return (
     <div className="App">
+      <AppNavbar/>
       <Dashboard/>
     </div>
   );
---
+++ class-21_and_22_func_component_router/src/components/navbar/navbar.component.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const AppNavbar = () => {
+    return(
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">App Name</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="/404">Error</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default AppNavbar;
