a0494910cbc87a343ca4eb01980005d733393330
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-22 22:48:32 +0600

    merge fix

--- class_24_youtube_clone/src/index.js
+++ class_24_youtube_clone/src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
-=======
-import ReactDOM  from 'react-dom/client';
->>>>>>> 63c81bd6c207d4f5573dff1b8d6f3cf1f996fee9
 
 import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-<<<<<<< HEAD
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-=======
-root.render(<App />);
->>>>>>> 63c81bd6c207d4f5573dff1b8d6f3cf1f996fee9
