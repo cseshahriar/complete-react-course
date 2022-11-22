5602eed7253f116b08eb96bd366a45dc90ede406
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-11-22 23:52:35 +0600

    search functionality

--- class_24_youtube_clone/src/App.js
+++ class_24_youtube_clone/src/App.js
@@ -6,15 +6,26 @@ import youtube from './api/youtube'; // export default youtube
 
 class App extends React.Component {
     
-    handleSubmit = () => {
-
+    handleSubmit = async(searchTerm) => {
+        const response = await youtube.get(
+            'search', 
+            {
+                params:{
+                    q: searchTerm,
+                    part: 'snippet',
+                    maxResults: 5,
+                    key:'AIzaSyCpqFb4UmogECg8b8xR0OMY8SRf_L3LgFI'
+                } 
+            }
+        );
+        console.log(response);
     }
 
     render() {
         return(
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs={12}>
-                <Grid container spacing={16}>
+                <Grid container spacing={10}>
                     
                     {/* Search bar */}
                     <Grid item xs={12}>
