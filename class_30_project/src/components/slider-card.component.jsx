f91a2888defd99a259763e188806d10b527bf266
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-13 17:54:39 +0600

    slider css fix

--- class_30_project/src/components/slider-card.component.jsx
+++ class_30_project/src/components/slider-card.component.jsx
@@ -5,13 +5,13 @@ const SliderCardComponent = (props) => {
     const {imgSrc, price, title} = props.data;
 
     return (
-        <Card className="p-0 overflow-hidden h-100 shadow">
-            <div className="overflow-hidden rounded p-0 bg-light">
+        <Card className="p-0 overflow-hidden h-100 shadow  bg-light">
+            <div className="overflow-hidden rounded p-0">
                 <Card.Img variant="top" src={imgSrc} />
             </div>
             <Card.Body className="text-center">
-                <Card.Title className="display-6">{price}</Card.Title>
                 <Card.Title className="display-6">{title}</Card.Title>
+                <Card.Title className="display-6">{price}</Card.Title>
             </Card.Body>
             <Button className="w-100 rounded-0" variant="success">Add To Cart</Button>
         </Card>
