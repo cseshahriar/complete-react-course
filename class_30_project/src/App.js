ef892483aa7172190177c1919727ae6bdcb88b0a
cseshahriar <cse.shahriar.hosen@gmail.com>
2022-12-13 11:03:15 +0600

    navbar component

--- class_30_project/src/App.js
+++ class_30_project/src/App.js
@@ -1,36 +1,11 @@
-import {forwardRef, useRef, useEffect} from "react";
+import React, {useEffect} from "react";
 import './App.css';
 
+import NavBarComponent from "./components/nav.component";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-            <Host/>
-      </header>
+        <NavBarComponent/>
     </div>
   );
 }
-
-function Host() {
-    const inpRef = useRef(null);
-
-    useEffect(() => {
-        inpRef.current.focus();
-    }, []);
-
-    return (
-        <MyInput type="text" ref={inpRef}>Hello World</MyInput>
-    );
-};
-
-const MyInput = forwardRef((props, ref) => {
-    return(
-        <>
-            <input ref={ref} />
-            {props.children}
-        </>
-    );
-});
-
-// useRef use for next/prev form
-// previous form data keep
---
+++ class_30_project/src/components/nav.component.jsx
@@ -0,0 +1,48 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCartPlus} from "@fortawesome/free-solid-svg-icons";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
+import {faCogs} from "@fortawesome/free-solid-svg-icons";
+
+function NavBarComponent() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">React E-Commerce</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Form className="d-flex">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search Products..."
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+
+                    <Nav className="m-auto">
+                        <Nav.Link href="/cart"><FontAwesomeIcon icon={faCartPlus} /> Cart</Nav.Link>
+                        <Nav.Link href="/dashboard"><FontAwesomeIcon icon={faCogs} />  Admin User</Nav.Link>
+
+                        <NavDropdown title="Username" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="/orders">Orders</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Settings</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBarComponent;
