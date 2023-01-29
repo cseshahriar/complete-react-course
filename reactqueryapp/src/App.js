a68beffa20363398d11e7c51a4d55abc71f81f18
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-01-29 22:39:21 +0600

    react quert fetch get request

--- reactqueryapp/src/App.js
+++ reactqueryapp/src/App.js
@@ -1,10 +1,33 @@
+import React, {useState, useEffect } from "react";
 import './App.css';
+import { useQuery } from 'react-query';
+
+const fetcher = (url) => {
+    return fetch(url)
+            .then(response => response.json())
+}
 
 function App() {
+    const [state, setState ] = useState(false);
+
+    const {isLoading, data, error } = useQuery(
+          ['github-data', 'facebook/react'],
+        () => fetcher("https://api.github.com/repos/facebook/react")
+      )
+
+    if(isLoading) {
+        return <h2>Loading...</h2>
+    }
+
+    if(error) {
+        return <h2>{error}</h2>
+    }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hellow World</h1>
+          <h1>React query</h1>
+            <p>{ data && data.name }</p>
       </header>
     </div>
   );
