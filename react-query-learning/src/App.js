5211742a0c9c6b5bcc05bf4e483f2431bb5a4361
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-06 17:29:12 +0600

    dependency installed

--- react-query-learning/src/App.js
+++ react-query-learning/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from 'react'
+import {
+    QueryClient,
+    QueryClientProvider,
+} from 'react-query'
+// 1. import `ChakraProvider` component
+import { ChakraProvider, Heading } from '@chakra-ui/react'
+
+// npm i react-query
+// npm i @chakra-ui/react @emotion/react @emotion/styled framer-motion formik formik-chakra-ui
+// npm i axios react-router-dom
+
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hello React Query</h1>
-        </div>
-    );
-};
 
-export default App;
+    const queryClient = new QueryClient()
+    return (
+        <ChakraProvider>
+            <QueryClientProvider client={queryClient}>
+                <Heading>
+                    Hello React Query
+                </Heading>
+            </QueryClientProvider>
+        </ChakraProvider>
+    )
+}
+export default App
