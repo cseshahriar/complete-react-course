a9d5c960018e6062a5a3c9e146e4f6b8a9b87fa4
cseshahriar <cse.shahriar.hosen@gmail.com>
2023-02-18 19:46:28 +0600

    array field

--- react-formik/src/App.js
+++ react-formik/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, FieldArray } from 'formik';
 
 function App() {
   return (
@@ -20,76 +20,103 @@ function App() {
                       social: {
                           facebook: '',
                           twitter: ''
-                      }
+                      },
+                      hobbies: [""]
                   }
                 }
                   onSubmit={(values) => {
                     console.log(values);
                   }}
               >
-                  <Form>
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="name">Name: </label>
-                          <Field name="name" id="name" type="text" className="form-control" />
-                      </div>
+                  {
+                      ({values }) => (
+                          <Form>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="name">Name: </label>
+                              <Field name="name" id="name" type="text" className="form-control" />
+                          </div>
 
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="phone">Phone: </label>
-                          <Field name="phone" type="tel" id="phone" className="form-control" />
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="phone">Phone: </label>
+                              <Field name="phone" type="tel" id="phone" className="form-control" />
+                          </div>
 
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="password">Password: </label>
-                          <Field name="password" type="password" id="password" autoComplete="on" className="form-control" />
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="password">Password: </label>
+                              <Field name="password" type="password" id="password" autoComplete="on" className="form-control" />
+                          </div>
 
-                      <div className="form-check">
-                          <label className="form-label">Gender: </label> <br/>
-                          <Field type="radio" name="gender" value="Male" id="male" />
-                          <label className="form-check-label" htmlFor="male">Male</label>
-                      </div>
+                          <div className="form-check">
+                              <label className="form-label">Gender: </label> <br/>
+                              <Field type="radio" name="gender" value="Male" id="male" />
+                              <label className="form-check-label" htmlFor="male">Male</label>
+                          </div>
 
-                      <div className="form-check">
-                          <Field type="radio" name="gender" value="Female" id="female" />
-                          <label className="form-check-label" htmlFor="female">Female</label>
-                      </div>
+                          <div className="form-check">
+                              <Field type="radio" name="gender" value="Female" id="female" />
+                              <label className="form-check-label" htmlFor="female">Female</label>
+                          </div>
 
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="dob">Date of Birth: </label>
-                          <Field name="dob" type="date" id="dob" className="form-control" />
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="dob">Date of Birth: </label>
+                              <Field name="dob" type="date" id="dob" className="form-control" />
+                          </div>
 
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="income">Income</label>
-                          <Field name="income" as="select" id="income" className="form-select">
-                            <option value="0">TK 0</option>
-                            <option value="1000">TK 1000</option>
-                            <option value="10000">TK 10000</option>
-                          </Field>
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="income">Income</label>
+                              <Field name="income" as="select" id="income" className="form-select">
+                                  <option value="0">TK 0</option>
+                                  <option value="1000">TK 1000</option>
+                                  <option value="10000">TK 10000</option>
+                              </Field>
+                          </div>
 
-                      <div className="form-group">
-                          <label className="form-label" htmlFor="about">Income</label>
-                          <Field name="about" as="textarea" id="about" className="form-control"/>
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="about">Income</label>
+                              <Field name="about" as="textarea" id="about" className="form-control"/>
+                          </div>
 
-                      <div className="form-group"> {/* nested field */}
-                          <label className="form-label" htmlFor="social">Social</label><br/>
+                          <div className="form-group"> {/* nested field */}
+                              <label className="form-label" htmlFor="social">Social</label><br/>
 
-                          <label className="form-label" htmlFor="social">Facebook</label>
-                          <Field name="social.facebook" type="text"  className="form-control"/>
+                              <label className="form-label" htmlFor="social">Facebook</label>
+                              <Field name="social.facebook" type="text"  className="form-control"/>
 
-                          <label className="form-label" htmlFor="social">Twitter</label>
-                          <Field name="social.twitter" type="text"  className="form-control"/>
+                              <label className="form-label" htmlFor="social">Twitter</label>
+                              <Field name="social.twitter" type="text"  className="form-control"/>
+                          </div>
 
-                      </div>
+                          <div className="form-group">
+                              <label className="form-label" htmlFor="hobbies">Hobbies</label>
+                              <FieldArray
+                                  name="hobbies"
+                                  render={
+                                      arrayHelpers => (
+                                          <>
+                                              <div>
+                                                  {
+                                                      values.hobbies.map((hobby, index) => (
+                                                          <div key={index}>
+                                                              <Field name={`hobbies.${index}`} />
+                                                              <button type="button" onClick={() => arrayHelpers.remove(index)}>-</button>
+                                                          </div>
+                                                      ))
+                                                  }
+                                              </div>
+                                              <button type="button" onClick={() => arrayHelpers.push('')}>Add Hobby</button>
+                                          </>
+                                      )
+                                }
+                              />
+                          </div>
 
-                      <div className="form-group mt-3 text-center">
-                          <button type="submit" className="btn btn-success">
-                              Submit
-                          </button>
-                      </div>
-                  </Form>
+                          <div className="form-group mt-3 text-center">
+                              <button type="submit" className="btn btn-success">
+                                  Submit
+                              </button>
+                          </div>
+                      </Form>
+                      )}
               </Formik>
           </div>
       </div>
